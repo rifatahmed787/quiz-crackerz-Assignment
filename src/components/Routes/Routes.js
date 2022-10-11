62b2fa1590a65c6688567cc2439a348efcfd1d97
Rifat <mdrifatahmed787@gmail.com>
2022-10-11 22:21:09 +0600

    handleNavigate button added

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Blog from "../Blog/Blog";
 import Home from "../Home/Home";
 import Main from "../Main/Main";
+import Quiz from "../Quiz/Quiz";
 import ShowError from "../ShowError/ShowError";
 
 
@@ -19,6 +20,13 @@ export const router=createBrowserRouter([
              path:'/home',
              loader: ()=>fetch('https://openapi.programming-hero.com/api/quiz'),
              element:<Home></Home>
+           },
+           {
+            path:'/topic/:topicId',
+            loader: async({params})=>{
+              return fetch(`https://openapi.programming-hero.com/api/quiz/${params.topicId}`)
+            }, 
+            element:<Quiz></Quiz>
            },
            {
             path:'/blog',
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Topic = ({topic}) => {
     // console.log(topic)
     const {logo, name}=topic;
+        
+    const navigate=useNavigate();
+    const handleNavigate=()=>{
+        navigate(`/topic/${topic.id}`);
+    }
+
     return (
     <div className="card w-60 h-80 bg-gray-200 shadow-xl">
         <figure><img src={logo} alt="/" /></figure>
         <h2 className="card-title mx-auto">{name}</h2>
-         <button className='btn-primary mx-10 mt-5 border rounded-lg'>Start Quiz</button>
+         <button onClick={handleNavigate} className='btn-primary mx-10 mt-5 border rounded-lg'>Start Quiz</button>
     </div>
     );
 };
